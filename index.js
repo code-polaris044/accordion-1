934ef18816d6d0f37fae6a0d5b54e23823c96d56
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-12-09 12:17:58 +0900

    test commit

--- index.js
+++ index.js
@@ -3,41 +3,52 @@
 
 const parentMenu = document.querySelectorAll(".menu > li > a");
 
-// for (let i = 0; i < parentMenu.length; i++) {
-//     parentMenu[i].addEventListener("click", function (e) {
-//         e.preventDefault();
-//         this.classList.toggle("active");
-//         this.nextElementSibling.classList.toggle("active");
-//     });
-// }
-
-function toggleMenu(e) {
+/**
+ * Toggle menu visibility.
+ * @param {MouseEvent} e - The click event.
+ */
+const toggleMenu = (e) => {
     e.preventDefault();
-    this.classList.toggle("active");
-    this.nextElementSibling.classList.toggle("active");
-}
 
-function addEventListeners() {
-    const parentMenu = document.querySelectorAll(".menu > li > a");
-    for (let i = 0; i < parentMenu.length; i++) {
-        parentMenu[i].addEventListener("click", toggleMenu);
+    const target = e.currentTarget;
+
+    if (target instanceof HTMLElement) {
+        target.classList.toggle("active");
+
+        const nextSibling = target.nextElementSibling;
+        if (nextSibling instanceof HTMLElement) {
+            nextSibling.classList.toggle("active");
+        }
     }
-}
+};
 
-function removeEventListeners() {
-    const parentMenu = document.querySelectorAll(".menu > li > a");
-    for (let i = 0; i < parentMenu.length; i++) {
-        parentMenu[i].removeEventListener("click", toggleMenu);
-    }
-}
+/**
+ * Add click event listeners to menu items.
+ */
+const addEventListeners = () => {
+    parentMenu.forEach((item) => {
+        // @ts-ignore
+        item.addEventListener("click", toggleMenu);
+    });
+};
 
-// 初期状態での設定
+/**
+ * Remove click event listeners from menu items.
+ */
+const removeEventListeners = () => {
+    parentMenu.forEach((item) => {
+        // @ts-ignore
+        item.removeEventListener("click", toggleMenu);
+    });
+};
+
+// Initial setup based on window width
 if (window.innerWidth <= 750) {
     addEventListeners();
 }
 
-// ウィンドウのリサイズ時に条件を判定し、イベントリスナーを追加・削除
-window.addEventListener("resize", function () {
+// Add or remove event listeners on window resize
+window.addEventListener("resize", () => {
     if (window.innerWidth <= 750) {
         addEventListeners();
     } else {
